4e8308ec6d159d7028b4a28155c3336646312248
KirillFisenko <79265463469@ya.ru>
2024-10-29 18:34:41 +0300

    К уроку 3.1 Управление БД https://stepik.org/lesson/1364141/step/1?unit=1380052

--- stepik/Program.cs
+++ stepik/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MySql.Data.MySqlClient;
+
+public class Program
+{
+    public static void Main()
+    {
+        string connectionString = "Server=localhost;Database=test;Uid=root;Pwd=m48kHz16bit%;";
+
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+            Console.WriteLine("Подключение открыто");
+            Console.WriteLine("Запросы к БД");
+        }
+        Console.WriteLine("Подключение закрыто");
+    }
+}
