84fa93f54b3a1025478931cc3b9149a32081bf07
KirillFisenko <79265463469@ya.ru>
2025-03-24 20:49:35 +0300

    +

--- stepik/Data/ApplicationDbContext.cs
+++ stepik/Data/ApplicationDbContext.cs
@@ -5,8 +5,6 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
 
-    public ApplicationDbContext() { }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         var config = new ConfigurationBuilder()
