d46901c25a7e87cae2f4a230e346aac0f2c9215e
KirillFisenko <79265463469@ya.ru>
2025-05-19 22:21:21 +0300

    +

--- stepik/Program.cs
+++ stepik/Program.cs
@@ -1,8 +1,11 @@
-﻿public class Program
+﻿using Microsoft.EntityFrameworkCore;
+
+public class Program
 {
     public static void Main()
     {
         using var dbContext = new ApplicationDbContext();
+        dbContext.Database.Migrate();
         var menu = new MainMenu();
         menu.Display();
         menu.HandleUserChoice();
